c2304e80a69689cf8ed5057ee1c6f140bf659bba
Akash <akashisgreat0000@gmail.com>
2025-02-01 02:00:56 +0530

    initial commit

---
+++ build.md
@@ -0,0 +1,8 @@
+./xopp2pdf-with-nested.sh input_folder output_folder(writer/data)
+
+tree data --noreport -X -o data.xml
+
+
+git add -A
+git commit -m "Updated"
+git push
---
+++ xopp2pdf-with-nested.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Define input and output base directories
+INPUT_DIR="$1"
+OUTPUT_DIR="$2"
+
+# Find all .xopp files and process them
+find "$INPUT_DIR" -type f -name "*.xopp" | while read -r file; do
+    # Get the relative path of the file
+     REL_PATH="${file#./}"  # Remove leading './' if present
+     #REL_PATH="${file#"$INPUT_DIR/"}"  # Get relative path
+
+    # Create the corresponding directory structure in new_file
+    OUTPUT_PATH="$OUTPUT_DIR/${REL_PATH%/*}"
+    mkdir -p "$OUTPUT_PATH"
+
+    # Convert .xopp file to PDF using xournalpp
+    xournalpp_link_tool -p "$OUTPUT_PATH/${REL_PATH##*/}.pdf" "$file"
+
+    #echo "Converted: $file -> $OUTPUT_PATH/${REL_PATH##*/}.pdf"
+done
+
